e25a27c095cf912e8bbfae838a6695bf6ca6a062
Duy K. Bui <duy@buifamily.info>
2024-04-10 16:54:34 -0400

    feat: use computed

--- islands/Counter.tsx
+++ islands/Counter.tsx
@@ -1,4 +1,5 @@
 import type { Signal } from "@preact/signals";
+import { computed } from "@preact/signals";
 import { Icon } from "../components/Icon.tsx";
 import { Input } from "../components/Input.tsx";
 import numeral from "numeral";
@@ -10,36 +11,32 @@ export interface CounterProps {
   currencyRate: Signal<number>;
 }
 
-export const calculateCost = (props: CounterProps) => {
-  const taxedPrice = props.buyingPrice.value * (1 + props.taxRate.value / 100);
-  console.log(`taxedPrice: ${taxedPrice}`);
-  const weightInOz = props.capacity.value / 6.25;
-  console.log(`weightInOz: ${weightInOz}`);
-  const weight = weightInOz / 16;
-  console.log(`weight: ${weight}`);
-  const freight = weight * 5; // TODO: freightRate
-  console.log(`freight: ${freight}`);
-  const custom = 6; // TODO: custom
-  console.log(`custom: ${custom}`);
-  const cost = taxedPrice + freight + custom;
-  console.log(`cost: ${cost}`);
-  return cost;
-};
-
-export const calculateCostInVnd = (props: CounterProps) => {
-  const cost = calculateCost(props);
-  return cost * props.currencyRate.value;
-};
-
-export const calculateFinalPriceInVnd = (props: CounterProps) => {
-  const cost = calculateCost(props);
-  const finalPrice = cost * 1.1; // TODO: profitRate
-  const finalPriceInVnd = finalPrice * props.currencyRate.value;
-  const roundedFinalPriceInVnd = Math.ceil(finalPriceInVnd / 50000) * 50000;
-  return roundedFinalPriceInVnd;
-};
-
 export default function Counter(props: CounterProps) {
+  const cost = computed(() => {
+    const taxedPrice = props.buyingPrice.value *
+      (1 + props.taxRate.value / 100);
+    console.log(`taxedPrice: ${taxedPrice}`);
+    const weightInOz = props.capacity.value / 6.25;
+    console.log(`weightInOz: ${weightInOz}`);
+    const weight = weightInOz / 16;
+    console.log(`weight: ${weight}`);
+    const freight = weight * 5; // TODO: freightRate
+    console.log(`freight: ${freight}`);
+    const custom = 6; // TODO: custom
+    console.log(`custom: ${custom}`);
+    const cost = taxedPrice + freight + custom;
+    console.log(`cost: ${cost}`);
+    return cost;
+  });
+  const finalPrice = computed(() => {
+    const finalPrice = cost.value * 1.1; // TODO: profitRate
+    const finalPriceInVnd = finalPrice * props.currencyRate.value;
+    const roundedFinalPriceInVnd = Math.ceil(finalPriceInVnd / 50000) * 50000;
+    return roundedFinalPriceInVnd;
+  });
+  const finalPriceInVnd = computed(() =>
+    numeral(finalPrice.value).format("0,0")
+  );
   return (
     <div class="flex flex-col gap-4 p-4">
       <Input
@@ -88,7 +85,7 @@ export default function Counter(props: CounterProps) {
         helpText="Đã bao gồm phụ thu hải quan và phí gởi theo cân nặng"
         filled
         readonly
-        value={numeral(calculateFinalPriceInVnd(props)).format("0,0")}
+        value={finalPriceInVnd}
       >
         <Icon name="cash-stack" slot="prefix">$</Icon>
         <code slot="suffix">₫</code>
